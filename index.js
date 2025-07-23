1f7b869e74b551319df27d0a4018dda592b63baf
SasmithaLochana <159742384+SasmithaLochana@users.noreply.github.com>
2025-07-23 21:58:40 +0530

    Short the logic to assign the correspond image to the user randomly

--- index.js
+++ index.js
@@ -3,38 +3,15 @@ function rollDice() {
   var randomNumber1 = Math.floor(Math.random() * 6) + 1;
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-  /* Check the number that got player 1 and assign the corresponding image */
-  if (randomNumber1 === 1) {
-    document.querySelector(".img1").setAttribute("src", "./images/dice1.png");
-  } else if (randomNumber1 === 2) {
-    document.querySelector(".img1").setAttribute("src", "./images/dice2.png");
-  } else if (randomNumber1 === 3) {
-    document.querySelector(".img1").setAttribute("src", "./images/dice3.png");
-  } else if (randomNumber1 === 4) {
-    document.querySelector(".img1").setAttribute("src", "./images/dice4.png");
-  } else if (randomNumber1 === 5) {
-    document.querySelector(".img1").setAttribute("src", "./images/dice5.png");
-  } else {
-    document.querySelector(".img1").setAttribute("src", "./images/dice6.png");
-  }
+  /* Assign the corresponding image for player 1 */
+  var randomDiceImage = "images/" + "dice" + randomNumber1 + ".png";
+  document.querySelector(".img1").setAttribute("src", randomDiceImage);
 
-  /* Check the number that got player 2 and assign the corresponding image */
-  if (randomNumber2 === 1) {
-    document.querySelector(".img2").setAttribute("src", "./images/dice1.png");
-  } else if (randomNumber2 === 2) {
-    document.querySelector(".img2").setAttribute("src", "./images/dice2.png");
-  } else if (randomNumber2 === 3) {
-    document.querySelector(".img2").setAttribute("src", "./images/dice3.png");
-  } else if (randomNumber2 === 4) {
-    document.querySelector(".img2").setAttribute("src", "./images/dice4.png");
-  } else if (randomNumber2 === 5) {
-    document.querySelector(".img2").setAttribute("src", "./images/dice5.png");
-  } else {
-    document.querySelector(".img2").setAttribute("src", "./images/dice6.png");
-  }
+  /* Aassign the corresponding image for player 2 */
+  var randomDiceImage2 = "images/" + "dice" + randomNumber2 + ".png";
+  document.querySelector(".img2").setAttribute("src", randomDiceImage2);
 
   /* Check who's the winner */
-
   if (randomNumber1 === randomNumber2) {
     document.querySelector("h1").innerHTML = "Draw! ❌";
   } else if (randomNumber1 > randomNumber2) {
